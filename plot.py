f50a4a8ab4710f4f7ee34d4ed55bb299f7cc9fec
Robbie Koevoets <robbie.koevoets@live.nl>
2022-10-17 16:54:01 +0200

    Update

---
+++ plot.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def read_data(filename):
+    return pd.read_csv(filename, delimiter=';', decimal=',')
+
+
+def plot_beer_data(data):
+    year_vals, _, consumption_vals = data.T.values
+
+    plt.figure()
+
+    plt.plot(year_vals, consumption_vals)
+
+    plt.title("Beer consumption in the Netherlands")
+    plt.xlabel("Year")
+    plt.ylabel("Consumption (x1000 hectoliter)")
+
+    with open("plot.png", "wb") as fp:
+        plt.savefig(fp)
+    plt.show()
+
+
+if __name__ == "__main__":
+    plt.rcParams["figure.dpi"] = 300
+    plt.rcParams["savefig.dpi"] = 300
+
+    plot_beer_data(read_data("istherecorrelation.csv"))
---
+++ solution_robbie_koevoets.md
@@ -0,0 +1,5 @@
+The Rise of Coccidioides: Forces Against the Dust Devil Unleashed
+An analysis of the forces required to drag sheep over various surfaces
+Deficits in speech perception predict language learning impairment
+
+![Beer consumption in the Netherlands plot](plot.png)
