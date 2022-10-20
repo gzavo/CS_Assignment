7317aa97dfa0905992d1f931478d2647bb12622f
sjdronkers <sjoerddronkers@live.nl>
2022-10-20 19:38:38 +0200

    fix

--- plot.py
+++ plot.py
@@ -3,8 +3,11 @@ import seaborn as sns
 import pandas as pd
 
 def plot() -> None:
-    df = pd.read_csv("istherecorrelation.csv")
-    df['NL Beer consumption [x1000 hectoliter]'] = df['NL Beer consumption [x1000 hectoliter]'].apply(lambda x: x/1000)
+    """
+    Plots the yearly beer consumption of the world vs The Netherlands.
+    """
+    df = pd.read_csv("istherecorrelation.csv", sep=';', decimal=',')
+    df["NL Beer consumption [x1000 hectoliter]"] = df["NL Beer consumption [x1000 hectoliter]"] / 1000
     df.plot(x="Year", y=["WO [x1000]", "NL Beer consumption [x1000 hectoliter]"],
         subplots=True, layout=(1,2))
     plt.suptitle("Beer consumption")
