3aa3b07152329decc9842b4791f8ba2eb0e693d6
ThomasGlansdorp <118111857+ThomasGlansdorp@users.noreply.github.com>
2023-09-27 16:52:21 +0200

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,18 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("istherecorrelation.csv", delimter = ";")
+data = pd.read_csv("istherecorrelation.csv", delimiter=";")
 
-plt.figure(dpi=300)
-plt.plot(data["Year"], data["WO [x1000]"])
-plt.xlabel('Year')
-plt.ylabel('WO [x1000]')
-plt.title('WO from 2006 to 2018')
-plt.show()
+fig, ax = plt.subplots(figsize=(10, 5), dpi=300)
 
-plt.figure(dpi=300)
-plt.plot(data["Year"], data["NL Beer consumption [x1000 hectoliter]"])
-plt.xlabel('Year')
-plt.ylabel('NL Beer consumption [x1000 hectoliter]')
-plt.title('NL Beer consumption from 2006 to 2018')
+plt.title("NL Beer consumption and WO per year", fontsize=20)
+
+ax2 = ax.twinx()
+ax.plot(data["Year"], data["WO [x1000]"], color='y')
+ax2.plot(data["Year"], data["NL Beer consumption [x1000 hectoliter]"], color='r')
+ax.set_xlabel('Year', fontsize=12)
+ax.set_ylabel('WO [x1000]', color='y', fontsize=12)
+ax2.set_ylabel('NL Beer consumption [x1000 hectoliter]', color='r', fontsize=12)
 plt.show()
