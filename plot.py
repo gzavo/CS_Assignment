039b06b9c20201715f2043241028b96a67413fb8
kaleem-01 <113911443+kaleem-01@users.noreply.github.com>
2023-09-27 21:21:01 +0200

    Create plot.py

---
+++ plot.py
@@ -0,0 +1,20 @@
+"""
+This file is created for the assignment for Academic Skills Course and plot the 
+"""
+
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+
+# Read the file
+df = pd.read_csv("istherecorrelation.csv", delimiter=";")
+# First few rows
+df.head()
+
+# Plot
+plt.plot(df["Year"], df["NL Beer consumption [x1000 hectoliter]"])
+plt.xlabel("Year")
+plt.ylabel("x1000 hectoliters")
+plt.title("Beer Consumption in the Netherlands")
+plt.show()
+
