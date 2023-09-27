fd66d1770118c578b73826fde6c5359a7a013055
sachagijsbers <sacha@gijsbers.tel>
2023-09-27 14:32:13 +0200

    changes to plotting csv data

--- solution.py
+++ solution.py
@@ -1,9 +1,29 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# import data from csv
 df = pd.read_csv("istherecorrelation.csv", delimiter = ";")
-plt.figure(dpi=300)
-plt.plot(df["WO [x1000]"], df["NL Beer consumption [x1000 hectoliter]"])
-plt.xlabel("WO [x1000]")
-plt.ylabel("NL Beer consumption [x1000 hectoliter]")
+
+# set plot parameters
+fig, ax = plt.subplots(figsize = (10, 5))
+plt.title('Correlation data of students and alcohol usage')
+
+# split data for two y axises
+ax2 = ax.twinx()
+ax.plot(df["Year"], df["WO [x1000]"], color = 'g')
+ax2.plot(df["Year"], df["NL Beer consumption [x1000 hectoliter]"], color = 'b')
+
+# giving labels to the axises
+ax.set_xlabel("Years", color = 'r')
+ax.set_ylabel("WO [x1000]", color = 'g')
+ax.plot(dpi=300)
+
+# secondary y-axis label
+ax2.set_ylabel("NL Beer consumption [x1000 hectoliter]", color = 'b')
+
+# defining display layout
+plt.tight_layout()
+
+# show plot
 plt.show()
+plt.savefig("correlation.png")
