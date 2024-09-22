18a22df53017c67a64d05599f70235390cd10f97
sofiatete <sofia.garcia.nunes@gmail.com>
2024-09-22 15:53:13 +0200

    code update

--- solution_.py
+++ solution_.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Loading the dataset 
+# Loading the dataset
 data = pd.read_csv('istherecorrelation.csv', delimiter=';')
+data['WO [x1000]'] = data['WO [x1000]'].str.replace(',', '.').astype(float)
 
-# Creating the figure and axis for WO
+# Creating the figure and the first axis (for WO)
 fig, ax1 = plt.subplots(figsize=(10, 6))
 
 # Plotting WO on the first y-axis
@@ -13,13 +14,21 @@ ax1.set_ylabel('WO [x1000]', color='blue')
 ax1.plot(data['Year'], data['WO [x1000]'], label='WO [x1000]', color='blue', marker='o')
 ax1.tick_params(axis='y', labelcolor='blue')
 
+# Setting a y-axis range for WO
+ax1.set_ylim([200, 300])  
+
 # Creating a second y-axis for NL Beer consumption
 ax2 = ax1.twinx()  
-ax2.set_ylabel('NL Beer consumption [x1000 hectoliter]', color='green')  
+ax2.set_ylabel('NL Beer consumption [x1000 hectoliter]', color='green')
 ax2.plot(data['Year'], data['NL Beer consumption [x1000 hectoliter]'], label='NL Beer Consumption', color='green', marker='o')
 ax2.tick_params(axis='y', labelcolor='green')
 
+# Setting a y-axis range for NL Beer consumption
+ax2.set_ylim([11000, 12500])  
+
+# Showing the plot
 plt.title('WO and NL Beer Consumption Over the Years')
 fig.tight_layout()  
 plt.savefig('wo_beer.png', dpi=300)
 plt.show()
+
