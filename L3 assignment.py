77488cdba5deacd677a09a24f82bbe16fd0d9180
Ruzutsas <Ruzutsas@users.noreply.github.com>
2024-09-23 12:50:57 +0200

    Solution

---
+++ L3 assignment.py
@@ -0,0 +1,28 @@
+import polars as pl
+import matplotlib.pyplot as plt
+
+df = pl.read_csv("istherecorrelation.csv", truncate_ragged_lines=True, separator=';', decimal_comma=True)
+
+plot = df.plot.line(x="WO [x1000]", y="NL Beer consumption [x1000 hectoliter]")
+
+xdata = df.get_column("Year")
+y1data = df.get_column("NL Beer consumption [x1000 hectoliter]")
+y2data = df.get_column("WO [x1000]")
+
+plt.rcParams['figure.dpi'] = 300
+fig, ax1 = plt.subplots()
+color = 'tab:blue'
+ax1.set_xlabel('Year')
+ax1.set_ylabel('NL Beer consumption [hectoliter]', color=color)
+ax1.plot(xdata, y1data, color=color)
+ax1.tick_params(axis='y', labelcolor=color)
+
+ax2 = ax1.twinx()
+color = 'tab:red'
+ax2.set_xlabel('Year', color=color)
+ax2.set_ylabel('WO [x1000]', color=color)
+ax2. plot(xdata, y2data, color=color)
+ax2.tick_params(axis='y', labelcolor=color)
+
+
+plt.show()
---
+++ solution_Leung_Tin.md
@@ -0,0 +1,10 @@
+StudentID: 15901475
+# Title of Papers
+MCC Van Dyke et al., 2019: Fantastic yeasts and where to find them: the hidden diversity of dimorphic fungal pathogens
+
+JT Harvey, Applied Ergonomics, 2002: An analysis of the forces required to drag sheep over various surfaces
+
+DW Ziegler et al., 2005: The neurocognitive effects of alcohol on adolescents and college students
+
+!["plot"](dpi_300.png)
+From this plot we can see that there is no correlation between beer consumption in the Netherlands and number of WO students, since the graphs have minimal overlap.
