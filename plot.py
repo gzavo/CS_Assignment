a3cc7ca9532773d21a083d570a6dff0248da1409
costadercole <costanza.dercole@student.uva.nl>
2024-09-25 13:08:15 +0200

    final version

--- plot.py
+++ plot.py
@@ -2,13 +2,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 dt = pd.read_csv('istherecorrelation.csv', delimiter=';')
-dt.columns = ["year", "wo_x1000", "nl_beerx1000"]
+dt.columns = ["year", "wox1000", "nl_beerx1000"]
+dt['wox1000'] = dt['wox1000'].str.replace(',', '.').astype(float)
 
 fig, ax1 = plt.subplots(figsize=(10, 6))
 
 ax1.set_xlabel('Year')
 ax1.set_ylabel('WO Beer Consumption (x1000 hectoliters)', color='tab:blue')
-ax1.plot(dt['year'], dt['wo_x1000'], marker='o', color='tab:blue', label='WO Beer Consumption')
+ax1.plot(dt['year'], dt['wox1000'], marker='o', color='tab:blue', label='WO Beer Consumption')
 ax1.tick_params(axis='y', labelcolor='tab:blue')
 
 ax2 = ax1.twinx()
@@ -17,7 +18,6 @@ ax2.plot(dt['year'], dt['nl_beerx1000'], marker='s', color='tab:orange', label='
 ax2.tick_params(axis='y', labelcolor='tab:orange')
 
 plt.title('WO vs NL Beer Consumption Over the Years')
-
 ax1.grid(True)
 
 lines_1, labels_1 = ax1.get_legend_handles_labels()
--- solution_.md
+++ solution_.md
@@ -6,4 +6,5 @@ Papers titles:
 
 Plot description:
 The plot shows the amount of beer consumption from 2006 to 2018 both for WO (university students in The Netherlands) as well as total NL consumption.
-It can bee seen that the consumption for university students had a constant increase over the years, while the total NL consumption had a slight decrease between 2009 and 2012, and then started increasing.
+It can be seen the plots have a similar behaviour, with beer consumption having a general increasing trend. However, total NL beer consumption shows a slight decrease from 2009 to 2012, while WO beer consumption lowered from 2012 to 2013.
+After 2013, the consumption for both groups had a steady increase.
